53055c653cc9c9da6f7874552fa7179ab1216c03
vaishnavi punati <85710302+uttej-t@users.noreply.github.com>
2023-11-10 11:15:30 -0800

    Created Main Page for Extracta Web Application,Added changed pahes of Global.css, Page.js, Page.Module.css

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,41 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+// page.js
+import Image from 'next/image';
+import styles from './page.module.css';
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div>
+      <header className={styles.header}>
+        <div className={styles.logoContainer}>
+          <Image src="/logo.png" alt="Logo" width={75} height={60} />
+          <h1 className={styles.logoTitle}>Extracta</h1>
         </div>
-      </div>
+        <nav className={styles.nav}>
+          <a href="/" className={styles.navItem}>Home</a>
+          <a href="/about" className={styles.navItem}>About</a>
+          <Link href="/try-now" className={styles.navButton}>Try Now
+          </Link>
+        </nav>
+      </header>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      <main className={styles.main}>
+        <div className={styles.contentContainer}>
+            <h1 className={styles.headline}>Unlock value from meetings, documents, and visual data.</h1>
+            <button className={styles.tryNowButton}>Try Now</button>
+        </div>
+        <div className={styles.illustration}>
+          <Image
+            src="/Home.svg"
+            alt="Illustration"
+            width={400} // set the appropriate size
+            height={400}
+            priority
+          />
+        </div>
+      </main>
+    </div>
+  );
 }
+
+
