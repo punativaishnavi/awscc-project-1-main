beb11bb3d3a8f6fbf1667b5bba83c22830fa38ee
vaishnavi punati <85710302+uttej-t@users.noreply.github.com>
2023-11-14 14:39:11 -0800

    Modified trynow button, to act as a link to service page

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,3 @@
-// page.js
 import Image from 'next/image';
 import styles from './page.module.css';
 import Link from 'next/link';
@@ -12,23 +11,32 @@ export default function Home() {
           <h1 className={styles.logoTitle}>Extracta</h1>
         </div>
         <nav className={styles.nav}>
-          <a href="/" className={styles.navItem}>Home</a>
-          <a href="/about" className={styles.navItem}>About</a>
-          <Link href="/try-now" className={styles.navButton}>Try Now
+          <a href="/" className={styles.navItem}>
+            Home
+          </a>
+          <a href="/about" className={styles.navItem}>
+            About
+          </a>
+          <Link href="/services" passHref>
+            <div className={styles.navButton}>Try Now</div>
           </Link>
         </nav>
       </header>
 
       <main className={styles.main}>
         <div className={styles.contentContainer}>
-            <h1 className={styles.headline}>Unlock value from meetings, documents, and visual data.</h1>
-            <button className={styles.tryNowButton}>Try Now</button>
+          <h1 className={styles.headline}>
+            Unlock value from meetings, documents, and visual data.
+          </h1>
+          <Link href="/services" passHref>
+            <div className={styles.tryNowButton}>Try Now</div>
+          </Link>
         </div>
         <div className={styles.illustration}>
           <Image
             src="/Home.svg"
             alt="Illustration"
-            width={400} // set the appropriate size
+            width={400}
             height={400}
             priority
           />
@@ -37,5 +45,3 @@ export default function Home() {
     </div>
   );
 }
-
-
